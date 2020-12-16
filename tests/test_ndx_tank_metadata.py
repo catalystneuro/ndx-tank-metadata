9c393ba73ef749c5210c8615e6864f8b52aa2310
weiglszonja <wszonja@gmail.com>
2020-12-16 17:34:53 +0100

    Test attributes are correct in nwbfile

--- tests/test_ndx_tank_metadata.py
+++ tests/test_ndx_tank_metadata.py
@@ -12,20 +12,17 @@ class LabMetaDataExtensionTest(unittest.TestCase):
         self.nwbfile = NWBFile('description', 'id', datetime.now().astimezone())
 
     def test_add_lab_metadata(self):
+        metadata_fields = dict(experiment_name='test',
+                               world_file_name='test',
+                               protocol_name='test',
+                               stimulus_bank_path='test',
+                               commit_id='test',
+                               location='test',
+                               session_performance=1.,)
+
         # Creates LabMetaData container
-
-        lab_metadata_dict = dict(
-            name='LabMetaData',
-            experiment_name='test',
-            world_file_name='test',
-            protocol_name='test',
-            stimulus_bank_path='test',
-            commit_id='test',
-            location='test',
-            session_performance=1.,
-        )
-
-        lab_metadata = LabMetaDataExtension(**lab_metadata_dict)
+        lab_metadata = LabMetaDataExtension(name='LabMetaData',
+                                            **metadata_fields)
 
         # Add to file
         self.nwbfile.add_lab_meta_data(lab_metadata)
@@ -36,9 +33,8 @@ class LabMetaDataExtensionTest(unittest.TestCase):
             io.write(self.nwbfile)
 
         with NWBHDF5IO(filename, mode='r', load_namespaces=True) as io:
-            nwbfile = io.read()
+            nwb = io.read()
 
-            for k, v in lab_metadata_dict.items():
-                self.assertEqual(v, getattr(nwbfile.lab_meta_data['LabMetaData'], k, None))
+        assert nwb.lab_meta_data['LabMetaData'].fields == metadata_fields
 
         os.remove(filename)
